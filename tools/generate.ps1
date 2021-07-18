68ada714c262dc9b91cf9d2e1a20afba08195aaa
Adarsh Github <adarsh@dli-pdc.com>
2021-07-18 05:37:03 +0530

    Deployed ba025de15 with MkDocs version: 1.2.1

--- tools/generate.ps1
+++
@@ -1,2 +0,0 @@
-# generate docs/compatibility-status.md
-invoke-expression -Command "$PSScriptRoot/generate-compatibility/generate.ps1" | Out-File -FilePath "$PSScriptRoot/../docs/compatibility-status.md" -Encoding UTF8
